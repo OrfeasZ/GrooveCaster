7c9b4b680a7a55a027cd5f142cae03443c296e87
Orfeas Zafeiris <orfeaz@gmail.com>
2015-03-18 20:27:55 +0200

    Implemented SuggestionManager, which allows easy management of song suggestions (useful for modules).

---
+++ GrooveCasterServer/Models/SimpleUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GrooveCaster.Models
+{
+    public class SimpleUser
+    {
+        public Int64 UserID { get; set; }
+
+        public String Name { get; set; }
+
+        public String ProfilePicture { get; set; }
+    }
+}
---
+++ GrooveCasterServer/Models/SongSuggestion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GrooveCaster.Models
+{
+    public class SongSuggestion
+    {
+        public Int64 SongID { get; set; }
+
+        public String SongName { get; set; }
+
+        public Int64 ArtistID { get; set; }
+
+        public String ArtistName { get; set; }
+
+        public Int64 AlbumID { get; set; }
+
+        public String AlbumName { get; set; }
+
+        public SimpleUser Suggester { get; set; }
+
+        public List<SimpleUser> OtherSuggesters { get; set; } 
+    }
+}
